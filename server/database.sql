897b939a221c40db104b89d1f672fe9a4b86235f
“Michelle <itsmichellelin@gmail.com>
2022-02-03 19:39:41 -0800

    server/database.sql

--- server/database.sql
+++ server/database.sql
@@ -8,60 +8,65 @@ CREATE TABLE tlp_user (
   username VARCHAR(255) UNIQUE,
   password VARCHAR(255),
   position pos,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255)
+  firstname VARCHAR(255),
+  lastname VARCHAR(255),
+  active BOOLEAN
 );
 
 
 CREATE TABLE administrator (
   admin_id SERIAL PRIMARY KEY,
-
+  CONSTRAINT id FOREIGN KEY (admin_id) REFERENCES tlp_user(user_id)
 );
 
 CREATE TABLE master_teacher (
   master_id SERIAL PRIMARY KEY,
+  CONSTRAINT id FOREIGN KEY (master_id) REFERENCES tlp_user(user_id)
 );
 
 
-CREATE TABLE school (
-    school_id SERIAL PRIMARY KEY,
-    school_name VARCHAR(255),
-    district_id INT,
-    CONSTRAINT district_id FOREIGN KEY(district_id)
-      REFERENCES school_district(district_id)
+CREATE TABLE site (
+    site_id SERIAL PRIMARY KEY,
+    site_name VARCHAR(255),
+    address_street VARCHAR(255),
+    address_city VARCHAR(255),
+    address_zip VARCHAR(5),
+    area_id INT,
+    CONSTRAINT area_id FOREIGN KEY(area_id)
+      REFERENCES site_area(area_id)
 );
 
-CREATE TABLE school_district(
-  district_id SERIAL PRIMARY KEY,
-  district_name VARCHAR(255)
+CREATE TABLE area (
+  area_id SERIAL PRIMARY KEY,
+  area_name VARCHAR(255),
+  active BOOLEAN
 );
 
 CREATE TABLE student (
     student_id SERIAL PRIMARY KEY,
-    school_id INT,
+    site_id INT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    CONSTRAINT school_id FOREIGN KEY(school_id)
-            REFERENCES school(school_id),
-    home teacher VARCHAR(255),
+    CONSTRAINT site_id FOREIGN KEY(site_id)
+      REFERENCES site(site_id),
+    home_teacher VARCHAR(255),
     student_groups INT[],
-    Pre-Test Reading Attitudes INT,
-    Post-Test Reading Attitudes INT,
-    Pre-Test Assessment Scores INT,
-    Post-Test Assessment Scores INT,
+    pretest_r INT[],
+    posttest_r INT[],
+    pretest_a INT[],
+    posttetst_a INT[]
 );
 
 CREATE TABLE student_group (
-  group_id SERIAL,
+  group_id SERIAL PRIMARY KEY,
   year INT,
-  PRIMARY KEY (group_id, yr),
-  master_id INT,
-  CONSTRAINT master_id FOREIGN KEY(master_id)
-    REFERENCES master_teacher(master_id),
   cycle season,
+  master_teacher INT,
+  CONSTRAINT master_teacher FOREIGN KEY(master_id)
+    REFERENCES master_teacher(master_id),
   students INT[],
-  CONSTRAINT school_id
-      FOREIGN KEY(school_id)
-          REFERENCES school(school_id),
+  site_id INT,
+  CONSTRAINT site_id FOREIGN KEY(site_id)
+    REFERENCES site(site_id),
   meeting_time DATE
 );
