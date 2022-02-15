acadf37b912b29b38e26406986c0624cce512ebc
Alan Chang <alan@alanchang.xyz>
2022-02-15 13:32:43 -0800

    Update database schema to 2/11 changes

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,12 @@
 CREATE DATABASE tlp;
 
-CREATE TYPE pos AS ENUM('admin', 'master teacher');
+CREATE TYPE pos AS ENUM('admin', 'master teacher', 'home teacher');
 CREATE TYPE season AS ENUM('winter', 'spring', 'summer', 'fall');
 CREATE TYPE user_status AS ENUM('active', 'inactive', 'pending');
 
 CREATE TABLE tlp_user (
   user_id SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  email VARCHAR(50) NOT NULL,
   position pos NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
@@ -16,14 +15,14 @@ CREATE TABLE tlp_user (
 );
 
 
-/*CREATE TABLE administrator (
-  admin_id SERIAL PRIMARY KEY,
-  CONSTRAINT id FOREIGN KEY (admin_id) REFERENCES tlp_user(user_id)
-);*/
+-- CREATE TABLE administrator (
+--   admin_id SERIAL PRIMARY KEY,
+
+-- );
 
 CREATE TABLE master_teacher (
-  teacher_id SERIAL PRIMARY KEY REFERENCES tlp_user(user_id),
-  area INT REFERENCES area(area_id) NOT NULL,
+  teacher_id SERIAL PRIMARY KEY REFERENCES tlp_user(user_id) ON DELETE CASCADE,
+  -- area INT REFERENCES area(area_id) NOT NULL,
   sites INT[]
 );
 
@@ -34,20 +33,21 @@ CREATE TABLE site (
     address_street VARCHAR(255) NOT NULL,
     address_city VARCHAR(255) NOT NULL,
     address_zip VARCHAR(5) NOT NULL,
-    area_id INT REFERENCES area(area_id) NOT NULL,
+    area_id INT REFERENCES area(area_id) ON DELETE CASCADE NOT NULL,
     p_first_name VARCHAR(255) NOT NULL,
     p_last_name VARCHAR(255) NOT NULL,
     p_title VARCHAR(255) NOT NULL,
-    p_num VARCHAR(255),
     p_phone_num VARCHAR(15) NOT NULL,
     p_email VARCHAR(255) NOT NULL,
     s_first_name VARCHAR(255),
     s_last_name VARCHAR(255),
     s_title VARCHAR(255),
-    s_num VARCHAR(255),
     s_phone_num VARCHAR(15),
     s_email VARCHAR(255) NOT NULL,
-    notes VARCHAR(255)
+    notes VARCHAR(255),
+    -- CONSTRAINT area_id FOREIGN KEY(area_id)
+    --   REFERENCES area(area_id)
+    --   ON DELETE CASCADE
 );
 
 CREATE TABLE area (
@@ -58,15 +58,15 @@ CREATE TABLE area (
 
 CREATE TABLE student (
     student_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    home_teacher INT REFERENCES tlp_user(user_id) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     site_id INT REFERENCES site(site_id) NOT NULL,
-    home_teacher VARCHAR(255) NOT NULL,
-    student_group INT[],
-    pretest_r INT[] NOT NULL,
-    posttest_r INT[] NOT NULL,
-    pretest_a INT[] NOT NULL,
-    posttetst_a INT[] NOT NULL
+    student_group INT REFERENCES student_group(group_id) NOT NULL,
+    pretest_r INT[],
+    posttest_r INT[],
+    pretest_a INT[],
+    posttetst_a INT[]
 );
 
 CREATE TABLE student_group (
