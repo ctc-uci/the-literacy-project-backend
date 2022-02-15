59b3af34c253e2117cb8d7eae2d29e196d939856
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-02-15 17:15:53 -0600

    remove database credentials (#20)
    * Create PR for #19
    
    * update database config file and other stuff
    
    * rename some variables
    
    Co-authored-by: Vakhshoori101 <Vakhshoori101@users.noreply.github.com>
    Co-authored-by: Vakhshoori101 <rvakhsho@uci.edu>

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+const loginRouter = require('./routes/login');
+const dashboardRouter = require('./routes/dashboard');
+const settingsRouter = require('./routes/settings');
+const teachersRouter = require('./routes/teachers');
+const sites = require('./routes/sites');
+const areas = require('./routes/areas');
+const students = require('./routes/students');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -12,6 +20,20 @@ app.use(
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
 );
+app.use(express.json()); // this gives us req.body
+
+// Database Schema
+// https://docs.google.com/document/d/11OQiiVDpT07Rk-jz0VY7yVKsAjqZC_97wewZR32cw6w/edit
+
+app.use('/dashboard', dashboardRouter);
+app.use('/login', loginRouter);
+app.use('/settings', settingsRouter);
+app.use('/teachers', teachersRouter);
+app.use('/sites', sites);
+app.use('/areas', areas);
+app.use('/students', students);
+// school district
+// schools
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
