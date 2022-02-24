94676944e987c36d459785cf49e41a3a06254489
Joshua Chan <joshualchan@gmail.com>
2022-02-23 16:06:52 -0800

    added get routes but not yet working

--- routes/sites.js
+++ routes/sites.js
@@ -3,34 +3,24 @@ const pool = require('../server/db');
 
 const router = Router();
 
-// Works!
-router.post('/create', async (req, res) => {
+router.post('', async (req, res) => {
   try {
     console.log(req.body);
     const siteInfo = req.body;
     const newSite = await pool.query(
-      'INSERT INTO site (site_name, address_street, address_city, address_zip, area_id, p_first_name, p_last_name, p_title, p_phone_num, p_email, s_first_name, s_last_name, s_title, s_phone_num, s_email, notes) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) RETURNING *',
+      'INSERT INTO site (site_name, address_street, address_city, address_zip, area_id, primary_contact_id, second_contact_id, notes) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *',
       [
         siteInfo.site_name,
         siteInfo.address_street,
         siteInfo.address_city,
         siteInfo.address_zip,
         siteInfo.area_id,
-        siteInfo.p_first_name,
-        siteInfo.p_last_name,
-        siteInfo.p_title,
-        siteInfo.p_phone_num,
-        siteInfo.p_email,
-        siteInfo.s_first_name,
-        siteInfo.s_last_name,
-        siteInfo.s_title,
-        siteInfo.s_phone_num,
-        siteInfo.s_email,
+        siteInfo.primary_contact_id,
+        siteInfo.second_contact_id,
         siteInfo.notes,
       ],
     );
     res.json(newSite.rows[0]);
-    // console.log(id);
   } catch (err) {
     console.error(err.message);
   }
@@ -39,7 +29,44 @@ router.post('/create', async (req, res) => {
 router.get('', async (req, res) => {
   try {
     const allSites = await pool.query('SELECT * FROM site');
+    console.log(allSites);
     res.json(allSites.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const site = await pool.query('SELECT * FROM site WHERE site_id = $1', [id]);
+    console.log(site);
+    res.json(site.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// not tested yet
+router.get('/:year', async (req, res) => {
+  try {
+    const { year } = req.params;
+    const sites = await pool.query('SELECT site_id FROM student_group WHERE year = $1', [year]);
+    res.json(sites.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// not tested yet
+router.get('/:year/:cycle', async (req, res) => {
+  try {
+    const { year, cycle } = req.params;
+    const sites = await pool.query(
+      'SELECT site_id FROM student_group WHERE year = $1 AND cycle = $2',
+      [year, cycle],
+    );
+    res.json(sites.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
