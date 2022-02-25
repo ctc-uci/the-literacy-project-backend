7d4ba1f6a8f8d9fcee363c247d7c47be2a39a80d
Alan Chang <alan@alanchang.xyz>
2022-02-25 03:03:07 -0800

    Prettify SQL queries on teachers routes

--- routes/teachers.js
+++ routes/teachers.js
@@ -7,7 +7,10 @@ router.post('/create', async (req, res) => {
   try {
     let teacherInfo = req.body;
     await pool.query(
-      'INSERT INTO general_user (first_name, last_name, phone_number, email, title) VALUES ($1, $2, $3, $4, $5)',
+      `INSERT INTO general_user
+        (first_name, last_name, phone_number, email, title)
+      VALUES
+        ($1, $2, $3, $4, $5)`,
       [
         teacherInfo.firstName,
         teacherInfo.lastName,
@@ -21,7 +24,11 @@ router.post('/create', async (req, res) => {
     ]); // This query is needed to fetch the user id for updating the tlp_user table
 
     const newTLPUser = await pool.query(
-      'INSERT INTO tlp_user (user_id, firebase_id, position, active) VALUES ($1, $2, $3, $4) RETURNING *',
+      `INSERT INTO tlp_user
+        (user_id, firebase_id, position, active)
+      VALUES
+        ($1, $2, $3, $4)
+      RETURNING *`,
       [
         teacherInfo.rows[0].user_id,
         'replace this later', // replace with actual firebase id; unfortunately this dummy value means only one user will be able to be made for now
@@ -31,7 +38,11 @@ router.post('/create', async (req, res) => {
     );
 
     const newMasterTeacher = await pool.query(
-      'INSERT INTO master_teacher (firebase_id, sites) VALUES ($1, $2) RETURNING *',
+      `INSERT INTO master_teacher
+        (firebase_id, sites)
+      VALUES
+        ($1, $2)
+      RETURNING *`,
       [newTLPUser.rows[0].firebase_id, []],
     );
 
@@ -44,7 +55,13 @@ router.post('/create', async (req, res) => {
 router.get('', async (req, res) => {
   try {
     const allTeachers = await pool.query(
-      'SELECT general_user.first_name, general_user.last_name, general_user.phone_number, general_user.email, tlp_user.active, master_teacher.sites FROM master_teacher INNER JOIN tlp_user ON tlp_user.firebase_id=master_teacher.firebase_id INNER JOIN general_user ON general_user.user_id=tlp_user.user_id',
+      `SELECT
+        general_user.first_name, general_user.last_name, general_user.phone_number, general_user.email,
+        tlp_user.active,
+        master_teacher.sites
+      FROM master_teacher
+        INNER JOIN tlp_user ON tlp_user.firebase_id=master_teacher.firebase_id
+        INNER JOIN general_user ON general_user.user_id=tlp_user.user_id`,
     );
     /* Resulting table rows will have:
     - First name
@@ -65,7 +82,14 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const allTeachers = await pool.query(
-      'SELECT general_user.first_name, general_user.last_name, general_user.phone_number, general_user.email, tlp_user.active, master_teacher.sites FROM master_teacher INNER JOIN tlp_user ON tlp_user.firebase_id=master_teacher.firebase_id INNER JOIN general_user ON general_user.user_id=tlp_user.user_id WHERE general_user.user_id=$1',
+      `SELECT
+        general_user.first_name, general_user.last_name, general_user.phone_number, general_user.email,
+        tlp_user.active,
+        master_teacher.sites
+      FROM master_teacher
+        INNER JOIN tlp_user ON tlp_user.firebase_id=master_teacher.firebase_id
+        INNER JOIN general_user ON general_user.user_id=tlp_user.user_id
+      WHERE general_user.user_id=$1`,
       [id],
     );
     /* Resulting table rows will have:
@@ -101,20 +125,25 @@ router.put('/:id', async (req, res) => {
   const { firstName, lastName, phoneNumber, email, active, sites } = req.body;
   console.log(req.body);
   try {
-    // Fix the values in general_user first
-    // Fix the values in tlp_user using userid
-    // Get the firebaseid from tlp_user, fix the sites in tlp_user
+    // Updating relevant values in general_user table
     await pool.query(
-      'UPDATE general_user SET first_name = $1, last_name = $2, phone_number = $3, email = $4 WHERE user_id = $5',
+      `UPDATE
+        general_user
+      SET
+        first_name = $1, last_name = $2, phone_number = $3, email = $4
+      WHERE
+        user_id = $5`,
       [firstName, lastName, phoneNumber, email, id],
     );
+    // Updating relevant values in tlp_user table
     await pool.query('UPDATE tlp_user SET active = $1 WHERE user_id = $2', [active, id]);
-    const teacherRow = await pool.query('SELECT * from tlp_user WHERE user_id = $1', [id]); // Getting the firebase id of the teacher
+    // Getting the firebase id of the teacher through the corresponding row object
+    const teacherRow = await pool.query('SELECT * from tlp_user WHERE user_id = $1', [id]);
+    // Updating relevant values in master_teacher table using the firebase_id
     await pool.query('UPDATE master_teacher SET sites = $1 WHERE firebase_id = $2', [
       sites,
       teacherRow.rows[0].firebase_id,
     ]);
-
     // Status 200 = request OK
     res.status(200);
     // res.send('This request actually succeeded!');
