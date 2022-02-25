ec3b18b758a0b3d367c3faee0ed9e86fc201566f
Hang Cao <hmcao1@uci.edu>
2022-02-24 16:56:57 -0800

    update queries to use user_id instead of firebase_id

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,34 @@ const pool = require('../server/db');
 
 const router = Router();
 
-// Returns all admins (first name, last name, phone number, email, active status)
+// to be updated with auth
+// creates admin with information in both general_user and tlp_user tables
+// Returns admin's info from general_user table
+router.post('', async (req, res) => {
+  try {
+    const { firebaseId, firstName, lastName, phoneNumber, email, active } = req.body;
+    const admin = await pool.query(
+      'INSERT INTO general_user(first_name, last_name, phone_number, email) VALUES ($1, $2, $3, $4) RETURNING *',
+      [firstName, lastName, phoneNumber, email],
+    );
+
+    await pool.query('INSERT INTO tlp_user VALUES ($1, $2, $3, $4)', [
+      admin.rows[0].user_id,
+      firebaseId,
+      'admin',
+      active,
+    ]);
+    res.status(200).send(admin.rows[0]);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Returns all admins (user_id, first name, last name, phone number, email, active status)
 router.get('', async (req, res) => {
   try {
     const allAdmins = await pool.query(
-      "SELECT genUser.first_name, genUser.last_name, genUser.phone_number, genUser.email, adminUser.active FROM (SELECT * FROM tlp_user WHERE position = 'admin') AS adminUser INNER JOIN general_user AS genUser ON adminUser.user_id=genUser.user_id",
+      "SELECT genUser.user_id, genUser.first_name, genUser.last_name, genUser.phone_number, genUser.email, adminUser.active FROM (SELECT * FROM tlp_user WHERE position = 'admin') AS adminUser INNER JOIN general_user AS genUser ON adminUser.user_id=genUser.user_id",
     );
     res.json(allAdmins.rows);
   } catch (err) {
@@ -15,38 +38,13 @@ router.get('', async (req, res) => {
   }
 });
 
-// to be updated with auth
-// creates admin with information in both general_user and tlp_user tables
-// id parameter is admin's firebase UID
-// Returns status 200 for successful creation
-router.post('', async (req, res) => {
-  try {
-    const { firebaseId, firstName, lastName, phoneNumber, email, active } = req.body;
-    // const { userId, firebaseId, active } = req.body;
-    const admin = await pool.query(
-      'INSERT INTO general_user(first_name, last_name, phone_number, email) VALUES ($1, $2, $3, $4) RETURNING *',
-      [firstName, lastName, phoneNumber, email],
-    );
-
-    const newAdmin = await pool.query('INSERT INTO tlp_user VALUES ($1, $2, $3, $4) RETURNING *', [
-      admin.rows[0].user_id,
-      firebaseId,
-      'admin',
-      active,
-    ]);
-    res.status(200).send(newAdmin.rows[0]);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-
-// id parameter is admin's firebase UID
+// id parameter is admin's user_id
 // Returns specified admin (first name, last name, phone number, email, active status)
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const selectedAdmin = await pool.query(
-      "SELECT genUser.first_name, genUser.last_name, genUser.phone_number, genUser.email, adminUser.active FROM (SELECT * FROM tlp_user WHERE position = 'admin' AND firebase_id = $1) AS adminUser INNER JOIN general_user AS genUser ON adminUser.user_id=genUser.user_id",
+      "SELECT genUser.first_name, genUser.last_name, genUser.phone_number, genUser.email, adminUser.active FROM (SELECT * FROM tlp_user WHERE position = 'admin' AND user_id = $1) AS adminUser INNER JOIN general_user AS genUser ON adminUser.user_id=genUser.user_id",
       [id],
     );
     res.json(selectedAdmin.rows);
@@ -56,36 +54,32 @@ router.get('/:id', async (req, res) => {
 });
 
 // updates the admin information in both general_user and tlp_user tables
-// id parameter is admin's firebase UID
+// id parameter is admin's user_id
 // Returns status 200 for successful update
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const { firstName, lastName, phoneNumber, email, active } = req.body;
-    const admin = await pool.query(
-      "UPDATE tlp_user SET active = $2 WHERE firebase_id = $1 AND position = 'admin' RETURNING *",
-      [id, active],
-    );
     await pool.query(
-      'UPDATE general_user SET first_name = $2, last_name = $3, phone_number = $4, email = $5 WHERE user_id = $1',
-      [admin.rows[0].user_id, firstName, lastName, phoneNumber, email],
+      'UPDATE general_user SET first_name = $1, last_name = $2, phone_number = $3, email = $4 WHERE user_id = $5',
+      [firstName, lastName, phoneNumber, email, id],
     );
+    await pool.query("UPDATE tlp_user SET active = $1 WHERE user_id = $2 AND position = 'admin'", [
+      active,
+      id,
+    ]);
     res.status(200).send(`Successfully updated admin: ${id}`);
   } catch (err) {
     res.status(400).send(err.message);
   }
 });
 
-// id parameter is admin's firebase UID
+// id parameter is admin's user_id
 // Returns status 200 for successful deletion
 router.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const userId = await pool.query(
-      "SELECT tlp_user.user_id FROM tlp_user WHERE firebase_id = $1 AND position = 'admin'",
-      [id],
-    );
-    await pool.query('DELETE FROM general_user WHERE user_id = $1', [userId.rows[0].user_id]);
+    await pool.query('DELETE FROM general_user WHERE user_id = $1', [id]);
     res.status(200).send(`Successfully removed admin: ${id}`);
   } catch (err) {
     res.status(400).send(err.message);
