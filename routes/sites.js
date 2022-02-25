087fbb2ae0a2f7586e63ca44a9dd13d334bfb257
Alan Chang <alan@alanchang.xyz>
2022-02-25 11:12:48 -0800

    Merge feature branch to resolve conflicts

--- routes/sites.js
+++ routes/sites.js
@@ -9,7 +9,7 @@ router.post('/create', async (req, res) => {
     console.log(req.body);
     const siteInfo = req.body;
     const newSite = await pool.query(
-      'INSERT INTO site (site_name, address_street, address_city, ss_city, address_zip, area_id, p_first_name, p_last_name, p_title, p_phone_num, p_email, s_first_name, s_last_name, s_title, s_phone_num, s_email, notes) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$16) RETURNING *',
+      'INSERT INTO site (site_name, address_street, address_city, address_zip, area_id, p_first_name, p_last_name, p_title, p_phone_num, p_email, s_first_name, s_last_name, s_title, s_phone_num, s_email, notes) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) RETURNING *',
       [
         siteInfo.site_name,
         siteInfo.address_street,
