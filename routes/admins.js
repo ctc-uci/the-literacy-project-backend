bd96c397f9387c8e7ab810b057de5837ebe24df3
Hang Cao <hmcao1@uci.edu>
2022-03-08 22:43:53 -0800

    delete user from firebase

--- routes/admins.js
+++ routes/admins.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { pool } = require('../server/db');
 const { isNumeric, isAlphaNumeric, isPhoneNumber, keysToCamel } = require('./utils');
+const firebaseAdmin = require('../firebase');
 
 const router = Router();
 
@@ -91,6 +92,8 @@ router.delete('/:adminId', async (req, res) => {
   try {
     const { adminId } = req.params;
     isNumeric(adminId, 'Id must be a Number');
+    const tlpUser = await pool.query(`SELECT * FROM tlp_user WHERE user_id = $1`, [adminId]);
+    await firebaseAdmin.auth().deleteUser(tlpUser.rows[0].firebase_id);
     const deletedAdmin = await pool.query(
       `DELETE FROM general_user WHERE user_id = $1
       RETURNING *;`,
