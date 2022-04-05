fc2a84944f25be60266c0b7ddf76219655150ae6
Vakhshoori101 <rvakhsho@uci.edu>
2022-04-04 19:59:47 -0700

    add area management endpoint

--- routes/areas.js
+++ routes/areas.js
@@ -11,6 +11,45 @@ use the get all areas request then filter based on year on frontend
 */
 
 // get an area by id
+router.get('/area-management', async (req, res) => {
+  try {
+    const areaInfo = await pool.query(`
+      SELECT area.*, site.num_sites, site.site_info, mt_info.num_mts, student_info.num_students
+      FROM area
+        LEFT JOIN
+          (SELECT site.area_id, COUNT(site.site_id) as num_sites, array_agg(json_build_object('site_id', site.site_id, 'site_name', site.site_name)) as site_info
+          FROM site
+          GROUP BY site.area_id)
+        AS site ON site.area_id = area.area_id
+        LEFT JOIN
+          (SELECT site.area_id, COUNT(*) as num_mts
+          FROM master_teacher_site_relation as mt
+            INNER JOIN
+              (SELECT site.site_id, site.area_id FROM site)
+            AS site on site.site_id = mt.site_id
+          GROUP BY site.area_id)
+        AS mt_info on mt_info.area_id = area.area_id
+        LEFT JOIN
+          (SELECT site.area_id, COUNT(*) as num_students
+          FROM student
+            INNER JOIN
+              (SELECT sg.group_id, sg.site_id
+              FROM student_group AS sg)
+            AS sg ON sg.group_id = student.student_group_id
+            INNER JOIN
+              (SELECT site.site_id, site.area_id
+              FROM site)
+            AS site ON site.site_id = sg.site_id
+          GROUP BY site.area_id)
+        AS student_info on student_info.area_id = area.area_id
+      ORDER BY area.area_name;`);
+    res.status(200).send(keysToCamel(areaInfo.rows));
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// get an area by id
 router.get('/:areaId', async (req, res) => {
   try {
     const { areaId } = req.params;
